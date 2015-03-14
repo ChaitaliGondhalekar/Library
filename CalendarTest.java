8818dc0c39d719fbd4980aed0cdc5fdc441b648e
Chaitali Gondhalekar <ChaitaliGondhalekar@users.noreply.github.com>
2015-03-14 12:15:07 -0400

    Create CalendarTest.java

---
+++ CalendarTest.java
@@ -0,0 +1,46 @@
+/**
+ * Submitted By
+ * Ying Chen
+ * Chaitali Gondhalekar
+ */
+package library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author David Matuszek
+ *
+ */
+public class CalendarTest {
+	
+	Calendar cal;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		cal = new Calendar();
+	}
+	
+	/**
+     * Test method for {@link library.Calendar#getDate()}
+     * and {@link library.Calendar#advance()}.
+     */
+    @Test
+    public void testCalendar() {
+        assertEquals(0, cal.getDate());
+        cal.advance();
+        assertEquals(1, cal.getDate());
+        cal.advance();
+        assertEquals(2, cal.getDate());
+        cal.advance();
+        cal.advance();
+        assertEquals(4, cal.getDate());
+    }
+
+
+}
